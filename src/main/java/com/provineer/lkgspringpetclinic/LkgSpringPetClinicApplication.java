19aa6fc9ecec31cfd6619a9e4eabe019f603ea18
pesterlig <pesterlig@gmail.com>
2019-06-11 19:52:06 -0400

    first commit

---
+++ src/main/java/com/provineer/lkgspringpetclinic/LkgSpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.provineer.lkgspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkgSpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LkgSpringPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/provineer/lkgspringpetclinic/LkgSpringPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.provineer.lkgspringpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LkgSpringPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
